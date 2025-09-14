3b29f4d72f95104307e188075024a1b837005225
jhonjiacomasi <jhon.jiacomasi@gmail.com>
2025-09-14 16:39:41 -0300

    criando estrutura projeto

---
+++ src/main/java/com/edu/gestor/EdugestorApplication.java
@@ -0,0 +1,13 @@
+package com.edu.gestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdugestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdugestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/gestor/EdugestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.gestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdugestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
