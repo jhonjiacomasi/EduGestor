b8b8df918bda8f02bfde4d9e3de61a940dfed3e7
jhonjiacomasi <jhon.jiacomasi@gmail.com>
2025-12-07 23:57:27 -0300

    Ajuste Cors config

---
+++ src/main/java/com/edu/gestor/config/CorsConfig.java
@@ -0,0 +1,13 @@
+package com.edu.gestor.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("http://localhost:8080", null)
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*");
+	}
+}
